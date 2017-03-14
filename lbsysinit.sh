c26ea86ea336d80152974c6663dd61e1f63e86a2
DanWolanski <wolanskd@yahoo.com>
2017-03-14 13:19:52 -0400

    Cleanup to lbsysinit.sh to prevent spam

--- lbsysinit.sh
+++ lbsysinit.sh
@@ -2,7 +2,9 @@
 # This script will add the iptables rule for udp
 #   it will also setup so it is run each time at start 
 #   by placing them in the /etc/iptables/rules.v4
-LOG=/dev/null
+LOG=lbsysinit.log
+echo "Starting LBSystemInit" > $LOG
+logger -t "lbsysinit.sh" "Starting LB System Initilization via lbsysinit.sh script"
 EXITONFAIL=0
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -28,7 +30,6 @@ try (){
 }
 
 echo
-
 step "Installing wget and unzip" 
 try yum -y install wget unzip
 
@@ -44,45 +45,45 @@ try cd LBDebugScripts-master
 step "Setting scripts to executable"
 try chmod +x *.sh
 
+step "Disabling ipv6"
+try ./disableipv6.sh
+
+step "Disabling firewall"
+try ./disablefirewall.sh
+
+step "Disabling messages rate limiting"
+try ./disableratelimiting.sh
+
+
+step "Disabling rp_filter"
+ try ./disablerpfilter.sh
+
+step  "Disabling selinux"
+try ./disableselinux.sh
+
+step  "Adding udp iptables rule"
+try ./setudprules.sh
+
+step "updating hosts file"
+try ./sethosts.sh
+
+step "Installing Required Packages"
+try ./installreqpackages.sh
+cat packageinstall.log >> $LOG
+
+step "Performing yum update"
+try yum -y update
+
+step "Getting latest jdk"
+try ./getjdk.sh
+
+step "Updating profile"
+try . ./setprofile.sh
+
+
+logger -t "lbsysinit.sh" "lbsysinit.sh script Initilization complete"
 echo
-echo "Disabling ipv6"
-./disableipv6.sh
-
-echo
-echo "Disabling firewall"
-./disablefirewall.sh
-
-echo
-echo "Disabling messages rate limiting"
-./disableratelimiting.sh
-
-echo "---------------------------------------------------------------"
-echo "Disabling rp_filter"
- ./disablerpfilter.sh
-
-echo "Disabling selinux"
-./disableselinux.sh
-
-echo "Adding udp iptables rule"
-./setudprules.sh
-
-echo "updating hosts file"
-./sethosts.sh
-
-echo "Installing Required Packages"
-./installreqpackages.sh
-
-echo "Performing yum update"
-yum -y update
-
-echo "Getting latest jdk"
-./getjdk.sh
-
-echo "Updating profile"
-. ./setprofile.sh
-
-echo
-echo "Process Complete"
-echo -e "${RED} Please Reboot before installing the LB Package${NC}"
+echo "Process Complete, see ${LOG} for more details"
+echo -e "${RED}Please Reboot before installing the LB Package${NC}"
 echo
 
